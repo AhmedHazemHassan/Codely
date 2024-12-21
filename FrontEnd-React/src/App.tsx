2ca28675785c90408593c4d3ccb4fae4b80c54f9
farahhhm <f.mohamed2292@nu.edu.eg>
2024-12-21 16:22:37 +0200

    App.tsx

--- FrontEnd-React/src/App.tsx
+++ FrontEnd-React/src/App.tsx
@@ -2,27 +2,38 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import CreateAccountPage from './CreateAccPage';
 import HomePage from './HomePage';
-import AddProject from './AddProjPage'; 
+import AddProject from './AddProjPage';
 import ProjectPage from './ProjectPage';
 import { useState } from 'react';
-import "./App.css";
+import './App.css';
 
-const sampleProjects = [
+interface Project {
+  name: string;
+  visibleTo: string;
+}
+
+const sampleProjects: Project[] = [
   { name: 'Project 1', visibleTo: 'Everyone' },
   { name: 'Project 2', visibleTo: 'Admin' },
 ];
 
 function App() {
-  const [projects, setProjects] = useState(sampleProjects); 
+  const [projects, setProjects] = useState<Project[]>(sampleProjects);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/create-account" element={<CreateAccountPage />} />
         <Route path="/home" element={<HomePage projects={projects} />} />
-        <Route path="/add-project" element={<AddProject setProjects={setProjects} projects={projects} />}  />
-        <Route path="/project/:projectName" element={<ProjectPage projects={projects} />} />
-        <Route path="/add-project" element={<AddProject setProjects={setProjects} projects={projects} />}  />      
+        <Route
+          path="/add-project"
+          element={<AddProject setProjects={setProjects} projects={projects} />}
+        />
+        <Route
+          path="/project/:projectName"
+          element={<ProjectPage projects={projects} />}
+        />
       </Routes>
     </Router>
   );
